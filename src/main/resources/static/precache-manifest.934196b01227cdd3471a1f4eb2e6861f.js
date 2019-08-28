4a4667a0fd5b74a09ac4ef52a9f2ccc204d0822d
Demetrious Robinson <drobinson@pivotal.io>
2019-08-27 17:05:51 -0700

    Create react gameForm and gameForm types

---
+++ src/main/resources/static/precache-manifest.934196b01227cdd3471a1f4eb2e6861f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a510f014d4e10ce997adb6f7b639323d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309a6e885c2b64588e9b",
+    "url": "/static/css/main.79430d25.chunk.css"
+  },
+  {
+    "revision": "7dd0459df0520add21e8",
+    "url": "/static/js/2.001ac8cc.chunk.js"
+  },
+  {
+    "revision": "309a6e885c2b64588e9b",
+    "url": "/static/js/main.6b45c531.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
---
+++ src/main/resources/static/precache-manifest.bb373df15c83c3e9ca6624c6ccfece82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033f8427e696ad51e5fc5ab9b68fcd17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee425bec424dc254ccb",
+    "url": "/static/css/main.79430d25.chunk.css"
+  },
+  {
+    "revision": "0a4765efad6a1d8b53ea",
+    "url": "/static/js/2.b5efa4fd.chunk.js"
+  },
+  {
+    "revision": "6ee425bec424dc254ccb",
+    "url": "/static/js/main.db595179.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
